a79960c6b37a2442d2ced6bc9eda29d7a16687c4
Alessandro Alterno <alternonolimit@gmail.com>
2019-04-15 16:33:09 +0100

    Added getter setter to Tester class

--- classes/Tester.js
+++ classes/Tester.js
@@ -1,16 +1,34 @@
 var method = Tester.prototype;
 
-function Tester(lastName, firstName) {
+function Tester(lastName, firstName, mail) {
     this.lastName = lastName;
+    this.firstName = firstName;
+    this.mail = mail;
+}
+
+
+method.setLastName = (lastName) => {
+    this.lastName = lastName;
+}
+
+method.setFirstName = (firstName) => {
     this.firstName = firstName;
 }
 
-method.getLastName = function () {
+method.setMail = (mail) => {
+    this.mail = mail;
+}
+
+method.getLastName = () => {
     return this.lastName;
 }
 
-method.getFirstName = function () {
+method.getFirstName = () => {
     return this.firstName;
+}
+
+method.getMail = () => {
+    return this.mail;
 }
 
 module.exports = Tester;
