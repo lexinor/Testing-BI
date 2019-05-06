5e6b0b086b2d780b7d6072470db9e27567284029
MaximeCertain <maxime-certain@hotmail.fr>
2019-05-06 17:24:10 +0200

    Added API functions for "Tester" table

--- server.js
+++ server.js
@@ -123,6 +123,93 @@ app.get('/testers/:uId', function(req, res) {
 });
 
 
+//-----------------------------------Fonctions Test -------------------------------------------------------------
+//Récupère tous les Testeurs
+app.get('/tests', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM test", function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            res.json(rows);
+        });
+    });
+}   );
+//Crée un nouveau Testeur
+app.post('/tests', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    obj = JSON.parse(JSON.stringify(req.body, null, " "));
+    con.connect(function(err) {
+        console.log(obj);
+        if (err) throw err;
+        var sql = mysql.format("INSERT INTO test (tDescription, tdateCreation, duration, catId) VALUES (?,?,?,?);", [obj.description, obj.dateCreation, obj.duration, obj.catId]);
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+    res.status(200).end('Test créé' );
+});
+//Supprime le test selon son id et les lignes le concernant en pariticiper, en test
+app.delete('/tests/:tId', function (req, res) {
+    res.setHeader("Content-Type","application/json; charset=utf8");
+    let id = req.params.tId;
+    con.connect(function (err) {
+        if(err) throw err;
+        let sql3 = mysql.format("DELETE FROM participer WHERE tId = ?",id);
+        let sql2 = mysql.format("DELETE FROM test WHERE tId = ?",id);
+        let sql = mysql.format("DELETE FROM Analyse WHERE tId = ? ", id);
+
+        con.query(sql,function (err, result,fields) {
+            if(err) throw err;
+            console.log(result.affectedRows);
+        });
+        con.query(sql3,function (err, result,fields) {
+            if(err) throw err;
+            console.log(result.affectedRows);
+        });
+        con.query(sql2,function (err, result,fields) {
+            if(err) throw err;
+            res.status(200).end("Nombre de lignes supprimée : " + result.affectedRows);
+        });
+
+    });
+});
+//modifie le test selon son id
+app.put('/tests/:uId',function (req,res) {
+    res.setHeader("Content-Type","application/json; charset=utf8");
+    obj = JSON.parse(JSON.stringify(req.body,null," "));
+    let id = req.params.uId;
+
+    con.connect(function (err) {
+        if(err) throw  err;
+        console.log(id);
+        console.log(obj);
+        let sql = mysql.format("UPDATE test SET tDescription = ?, tdateCreation = ?, duration = ?, catId = ? WHERE tId = ?",[obj.description, obj.dateCreation,
+            obj.duration, obj.catId, id]);
+        con.query(sql,function (err, result) {
+            if(err) throw err;
+            res.status(200).end("Nombre de lignes modifiés: " + result.affectedRows);
+        })
+    })
+});
+
+//affiche un test particulier
+app.get('/tests/:tId', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    con.connect(function(err) {
+        if (err) throw err;
+        let Id = req.params.tId;
+        let sql = mysql.format("SELECT * FROM test WHERE tId=?",Id);
+        con.query(sql, function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            res.json(rows);
+        });
+    });
+});
 
 
 
