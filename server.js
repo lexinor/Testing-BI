944eea9022bc90918cc035bff64cf530d1c0fb19
MaximeCertain <maxime-certain@hotmail.fr>
2019-05-13 14:39:46 +0200

    Added API functions for "Tester" table

--- server.js
+++ server.js
@@ -217,6 +217,88 @@ app.get('/tests/:tId', function(req, res) {
 });
 
 
+//-----------------------------------Fonctions Execute -------------------------------------------------------------
+//Récupère toutes les éxécutions de test par un testeur
+app.get('/execute', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM execute", function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            res.json(rows);
+        });
+    });
+}   );
+//Crée une nouvelle éxécution
+app.post('/execute', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    obj = JSON.parse(JSON.stringify(req.body, null, " "));
+    con.connect(function(err) {
+        console.log(obj);
+        if (err) throw err;
+        var sql = mysql.format("INSERT INTO execute (uId, tId, cId, statut, startDate, endDate) " +
+            "VALUES (?,?,?,?,?,?);", [obj.uId, obj.tId, obj.cId, obj.statut, obj.startDate,obj.endDate]);
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+    res.status(200).end('Execution créée' );
+});
+
+//Supprime l'éxécution selon son Id
+app.delete('/execute/:uId/:tId/:cId', function (req, res) {
+    res.setHeader("Content-Type","application/json; charset=utf8");
+    con.connect(function (err) {
+        if(err) throw err;
+
+        let sql = mysql.format("DELETE FROM Execute WHERE uId = ? and tId = ? and cId = ? ; ", [req.params.uId,
+            req.params.tId,
+            req.params.cId]);
+
+        con.query(sql,function (err, result,fields) {
+            if(err) throw err;
+            res.status(200).end("Nombre de lignes supprimée : " + result.affectedRows);
+        });
+    });
+});
+//modifie l'éxécution selon son id
+app.put('/execute/:uId/:tId/:cId',function (req,res) {
+    res.setHeader("Content-Type","application/json; charset=utf8");
+    obj = JSON.parse(JSON.stringify(req.body,null," "));
+    con.connect(function (err) {
+        if(err) throw  err;
+        console.log(obj);
+        let sql = mysql.format("UPDATE Execute SET statut = ?, startDate = ?, endDate = ? where uId = ? and tId = ? and cId = ?",
+            [obj.statut,
+            obj.startDate, obj.endDate, req.params.uId,req.params.tId,req.params.cId]);
+        console.log(sql);
+        con.query(sql,function (err, result) {
+            if(err) throw err;
+            res.status(200).end("Nombre de lignes modifiés: " + result.affectedRows);
+        })
+    })
+});
+
+//affiche un test particulier
+app.get('/execute/:uId/:tId/:cId', function(req, res) {
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    con.connect(function(err) {
+        if (err) throw err;
+        let sql = mysql.format("SELECT * FROM Execute WHERE uId=? and tId = ? and cId = ? ", [req.params.uId,
+            req.params.tId,
+            req.params.cId]);
+        con.query(sql, function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            res.json(rows);
+        });
+    });
+});
+
+
 
 
 
